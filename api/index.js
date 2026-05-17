472b452590b11b641d3d877ba4c4b22891e5ff8e
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-16 21:31:07 -0300

    chore: update shareCode to G8OFYM83Q16

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=6S9169Y63PJ&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=G8OFYM83Q16&afp=tapi2' });
     res.end();
   }
