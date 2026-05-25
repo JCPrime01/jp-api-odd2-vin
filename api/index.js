4a4b6191e5735460146d68ecca0bdac8fe3e6b05
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-25 20:33:24 -0300

    chore: update shareCode to N939CCKFMTD

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=9UDEE27DXJV&afp=tapi2' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=N939CCKFMTD&afp=tapi2' });
     res.end();
   }
